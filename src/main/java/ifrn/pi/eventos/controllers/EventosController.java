9aa10536d9acc0bdc05e61688d8ec99c9970afd0
Jose Arthur Da silva <20211204010072@ifrn.local>
2024-11-18 10:51:48 -0300

    Apagando um evento da aplicação

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -4,8 +4,13 @@ package ifrn.pi.eventos.controllers;
 import java.util.List;
 import java.util.Optional;
 
+
+import ifrn.pi.eventos.models.Convidado;
+import ifrn.pi.eventos.models.Evento;
+import ifrn.pi.eventos.repositories.ConvidadoRepository;
+import ifrn.pi.eventos.repositories.EventoRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,13 +18,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import ifrn.pi.eventos.models.Convidado;
-import ifrn.pi.eventos.models.Evento;
-import ifrn.pi.eventos.repositories.ConvidadoRepository;
-import ifrn.pi.eventos.repositories.EventoRepository;
-
-
 
 
 @Controller
